a8ab2230e18fb364f5f89232284cbb200e601a05
0xshikhar <64684053+0xshikhar@users.noreply.github.com>
2025-07-30 21:22:36 +0530

    chore(lint): Update ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,15 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    "@typescript-eslint/no-explicit-any": "warn",
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-empty-object-type": "warn",
+      "@typescript-eslint/ban-ts-comment": "warn",
+      "prefer-const": "off", // disable prefer-const
+
+    },
   },
 ];
 
