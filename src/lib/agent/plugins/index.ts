bf9aba35db993f4f25c0f287449b6c75d3798101
0xshikhar <64684053+0xshikhar@users.noreply.github.com>
2025-10-23 02:22:52 +0530

    chore(agent): add initial scaffolding for Hedera agent

---
+++ src/lib/agent/plugins/index.ts
@@ -0,0 +1,6 @@
+// This file will export all plugins
+
+// export * from './DatasetCreationPlugin';
+// export * from './MarketplacePlugin';
+// export * from './VerificationPlugin';
+// export * from './ProviderManagementPlugin';
